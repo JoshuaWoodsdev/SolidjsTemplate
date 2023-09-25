0ef7a87bd28e6d829e3492070a9db8a88ee425cf
Joshua Woods developer <nordev@protonmail.com>
2023-09-25 14:27:37 -0400

    setup

---
+++ src/entry.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+    <h1>hi</h1>
+    </div>
+  );
+}
+
+document.getElementById('app').appendChild(App());
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // ... other loaders
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+  },
+};
